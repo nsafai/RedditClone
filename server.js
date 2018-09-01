426e166db8e775c7c1db2fc196b6977c28cc42b5
Nicolai Safai <nicolaisafai@gmail.com>
2018-08-31 17:11:34 -0700

    initial server setup showing hello world on root

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
 app.listen(3000, function () {
-  console.log('adding express to server.js on localhost:3000!');
+  console.log('Website listening on port 3000!');
 });
