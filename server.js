ccbe81db61537a443ccd3059cccc206a8f0431a4
Nicolai Safai <nicolaisafai@gmail.com>
2018-09-01 12:25:10 -0700

    added navbar

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.render('home')
 });
 
 app.listen(3000, function () {
   console.log('Website listening on port 3000!');
 });
+
+// example code for a GET route with placeholder for variable 'name'
+// app.get('/greetings/:name', function (req, res) {
+//  var name = req.params.name;
+//  res.render('greetings', {name: name});
+// })
